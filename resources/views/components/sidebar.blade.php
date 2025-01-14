36e96df8060c9d38d02551ae953c45be4e037aaf
RefliyantoKasim <refliyanto.mhs22@ti.poligon.ac.id>
2025-01-14 12:00:25 +0800

    meubel

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,36 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">MEUBEL</a>
+            <a href="index.html"><img src="{{ asset('img/kedai.png') }}" alt="logo" width="80"></a>
+
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">MK</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="{{ Request::is('home*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ url('home') }}"><i
+                        class="fas fa-chart-line"></i><span>Dashboard</span></a>
+            </li>
+            <li class="menu-header">Users</li>
+            <li class="{{ Request::is('user*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('user.index') }}"><i class="fas fa-user"></i><span>All
+                        Users</span></a>
+            </li>
+
+            <li class="menu-header">Product</li>
+            <li class="{{ Request::is('product*') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('product.index') }}"><i class="fas fa-box"></i><span>All
+                        Products</span></a>
+
+            </li>
+            <li class="menu-header">Orders</li>
+            <li class="nav-item">
+                <a href="{{ route('order.index') }}" class="nav-link {{ Request::is('order') ? 'active' : '' }}"><i
+                        class="fas fa-shopping-cart"></i><span>Orders</span></a>
+            </li>
+
+    </aside>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Profiler\Profile;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages.auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+
+    Route::resource('user', UserController::class);
+    Route::resource('product', ProductController::class);
+
+    Route::resource('order', OrderController::class);
+    Route::resource('profile', ProfileController::class);
+});
